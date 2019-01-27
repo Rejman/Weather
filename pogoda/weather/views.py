c8415b8d07aa36fe2fa35a21f40a98abf82e9076
Konrad Rejman <rreejjmmaann@gmail.com>
2019-01-27 11:54:54 +0100

    dodano predykcję oraz zmieniono wygląd

--- pogoda/weather/views.py
+++ pogoda/weather/views.py
@@ -4,7 +4,8 @@ import datetime
 
 def index(request):
     trials = Trial.objects.all()
-    now = datetime.datetime.now().strftime("%d-%m-%Y")
+    now = datetime.datetime.now().strftime("%m")
+    
     yearsSet = set()
     for trial in trials:
         yearsSet.add(int(trial.year))
@@ -13,7 +14,42 @@ def index(request):
         years.append(year)
     years.sort()
 
-    return render(request, 'weather/index.html', {'years': years, 'now':now})
+    trials = Trial.objects.filter(month=int(now))
+    months = {
+        "01":["Styczeń", 31],
+        "02":["Luty", 30],
+        "03":["Marzec", 31],
+        "04":["Kwiecień", 30],
+        "05":["Maj", 31],
+        "06":["Czerwiec", 30],
+        "07":["Lipiec", 31],
+        "08":["Sierpień", 31],
+        "09":["Wrzesień", 30],
+        "10":["Październik", 31],
+        "11":["Listopad", 30],
+        "12":["Grudzień",31],
+    }
+    now = months[now]
+    temperatures = []
+    falls = []
+    for trial in trials:
+        temperatures.append(float(trial.temp))
+        falls.append(trial.fall)
+
+    temperature = round(sum(temperatures)/len(temperatures),1)
+    f = sum(falls)/len(falls)
+    fall = round((f/now[1])*100)
+    
+    context={
+        'fall':fall,
+        'temperature':temperature,
+        'years': years,
+        'now':now[0],
+        'temperatures':temperatures,
+        'falls':falls
+        }
+
+    return render(request, 'weather/index.html', context)
 
 def year(request, year):
     trials = Trial.objects.filter(year=year)
@@ -35,19 +71,7 @@ def year(request, year):
         
     return render(request, 'weather/year.html', context)
 
-#def prediction(request):
-#    now = datetime.datetime.now().strftime("%m")
-#    trials = Trial.objects.all()
-#    
-#    temperatures = []
-#    falls = []
-#    for trial in trials:
-#        temperatures.append(float(trial.temp))
-#        falls.append(trial.fall)
 
-    
-    
-#    return render(request, 'weather/prediction.html', {'now':now})
     
 
     
